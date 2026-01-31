949d96b575fa98d1b05ae42d7a62fd713fbd5edc
Taneeshk Patel <taneeshkpatel08@gmail.com>
2026-01-31 18:22:08 +0530

    update

--- lib/supabase/client.ts
+++
@@ -1,4 +0,0 @@
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import type { Database } from '@/types/supabase';
-
-export const createClient = () => createClientComponentClient<Database>();
