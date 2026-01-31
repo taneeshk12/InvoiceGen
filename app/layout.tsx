6a1886bd4648f69209cf5a70544b5b11fa626805
Taneeshk Patel <taneeshkpatel08@gmail.com>
2026-01-31 18:24:25 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/react"
 import { ThemeProvider } from '@/components/theme-provider';
 import { ToastProvider } from '@/components/toast-provider';
 import './globals.css';
@@ -90,6 +91,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
           <ToastProvider />
         </ThemeProvider>
       </body>
